96c59b7c200d83856d4e8f3565c6b179f8d3a540
Alejandra B <alejandrabautista@MacBook-Pro-de-Alejandra.local>
2015-01-17 23:46:40 -0600

    Initial Commit

---
+++ TareaObjC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TareaObjC
+//
+//  Created by Alejandra B on 17/01/15.
+//  Copyright (c) 2015 Alejandra B. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
